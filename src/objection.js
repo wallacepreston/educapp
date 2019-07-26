c701fad3ab7bddd566dc5fa355e8dcf2db3b1d41
Preston Wallace <wallace.preston@gmail.com>
2019-07-26 11:44:01 -0700

    add dotenv to read env vars

--- src/objection.js
+++ src/objection.js
@@ -1,10 +1,11 @@
 const { Model } = require('objection');
+require('dotenv').config();
 
 module.exports = function (app) {
   const { client, connection } = app.get('postgres');
   const knex = require('knex')({ 
     client, 
-    connection, 
+    connection: process.env.DATABASE_URL || connection, 
     useNullAsDefault: false,
     migrations: {
       directory: __dirname + '/src/migrations',
