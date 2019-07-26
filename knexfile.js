308e4e5604c78162f5888a8924aa0593fb7e4e9d
Preston Wallace <wallace.preston@gmail.com>
2019-07-26 11:21:51 -0700

    edit knexfile to allow production

--- knexfile.js
+++ knexfile.js
@@ -13,20 +13,18 @@ module.exports = {
     seeds: {
       directory: __dirname + '/src/seeds'
     },
-    production: {
-      client: 'pg',
-      useNullAsDefault: true,
-      connection: {
-        database: pkg.name,
-      },
-      migrations: {
-        directory: __dirname + '/src/migrations',
-      },
-      seeds: {
-        directory: __dirname + '/src/seeds'
-      }
-      // useNullAsDefault: true,
-      // debug: true,
-    } 
-  }
+  },
+  production: {
+    client: 'pg',
+    useNullAsDefault: true,
+    connection: {
+      database: pkg.name,
+    },
+    migrations: {
+      directory: __dirname + '/src/migrations',
+    },
+    seeds: {
+      directory: __dirname + '/src/seeds'
+    }
+  } 
 };
